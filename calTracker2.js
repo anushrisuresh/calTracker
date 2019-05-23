6cc6d2410e5da94edc82883948edb3cb08f52bb4
Anushri Suresh <anushrisuresh@gmail.com>
2019-05-23 16:50:06 +0530

    got js2

---
+++ calTracker2.js
@@ -0,0 +1,39 @@
+function calculateTotalCalories()
+{
+	let bCarbs = document.getElementById("bCarbs").value;
+	let lCarbs = document.getElementById("lCarbs").value;
+	let dCarbs = document.getElementById("dCarbs").value;
+
+	let bProteins = document.getElementById("bProteins").value;
+	let lProteins = document.getElementById("lProteins").value;
+	let dProteins = document.getElementById("dProteins").value;
+
+	let bFats = document.getElementById("bFats").value;
+	let lFats = document.getElementById("lFats").value;
+	let dFats = document.getElementById("dFats").value;
+
+
+console.log(bCarbs);
+console.log(dProteins);
+console.log(lFats);
+
+ var total = 4*(bCarbs + lCarbs + dCarbs + bProteins + lProteins + dProteins) + 9*(bFats + lFats +dFats);
+
+ document.getElementById("calorieTotal").innerHTML= total;
+
+let result = document.getElementById("output")
+
+if(total>result){
+	document.getElementById("lessOrMore").innerHTML= 'You have exceeded your calorie limit by' + (total-result) + '. You might want to go get some exercise :p';
+
+}
+else if(total = result){
+	document.getElementById("lessOrMore").innerHTML= 'Your calorie consumption is perfect!';
+}
+else{
+	document.getElementById("lessOrMore").innerHTML=  "You haven't met your calorie requirement by" + (result-total)+ ". You can grab something to eat.";
+
+}
+
+
+}
